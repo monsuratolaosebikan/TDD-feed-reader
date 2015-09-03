da2278370fc3b57934125adfbb2def43d71f1108
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2015-09-02 21:50:12 -0400

    Add test for checking if each rss feed has a defined name that is not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,12 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('has url', function() {
+        it('have url', function() {
 
             for(var i=0; i<allFeeds.length; i++) {
             expect(allFeeds[i].url).toBeDefined();
             expect(allFeeds[i].url).not.toBe("");
         }
+
          });
 
 
@@ -44,6 +45,15 @@ $(function() {
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('have name', function() {
+
+            for(var i=0; i<allFeeds.length; i++) {
+            expect(allFeeds[i].name).toBeDefined();
+            expect(allFeeds[i].name).not.toBe("");
+        }
+        
+         });
+
     });
 
 
