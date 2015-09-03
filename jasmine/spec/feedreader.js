cc6bba41621a8562b653f3fd304a32487011b4fe
Monsurat Olaosebikan <monsuratolaosebikan@users.noreply.github.com>
2015-09-03 13:31:27 -0400

    Add test for checking if a new entry is added to the feed container when loadFeed() is called

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -74,21 +74,31 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-          it('changes visibility on click', function() {
+         it('changes visibility on click', function() {
             $('.menu-icon-link').click()
             expect($('body').hasClass('menu-hidden')).toBe(false);
             $('.menu-icon-link').click()
             expect($('body').hasClass('menu-hidden')).toBe(true);
-          });
+         });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
+   
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(0,done);
+         });
+         
+        it('has a single .entry element within the .feed container',function() {
+            expect($('.feed').hasClass('.entry')).toBe(true);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
