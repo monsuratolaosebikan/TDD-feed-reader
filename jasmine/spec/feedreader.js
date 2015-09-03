7bced8174a1e8cc7e523231688d8044c4b24426d
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2015-09-03 19:36:00 -0400

    Add test for checking if a new entry is added to the feed container when loadFeed() is called

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -82,13 +82,21 @@ $(function() {
           });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', function() {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done){
+            loadFeed(0,done);
+         });
+         
+        it('has a single .entry element within the .feed container',function() {
+            expect($('.feed .entry').length).not.toBe(0);
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection"
 
