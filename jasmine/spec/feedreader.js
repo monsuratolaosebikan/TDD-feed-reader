70cabf7a0835c4e133684902ed43b7029ea05bad
Monsurat Olaosebikan <monsuratolaosebikan@gmail.com>
2015-09-02 21:30:13 -0400

    Add test for checking that each rss feed has a defined url and is not empty

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,7 +31,14 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('has url', function() {
+
+            for(var i=0; i<allFeeds.length; i++) {
+            expect(allFeeds[i].url).toBeDefined();
+            expect(allFeeds[i].url).not.toBe("");
+        }
         
+         });
 
 
         /* TODO: Write a test that loops through each feed
